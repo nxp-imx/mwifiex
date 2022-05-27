2365e637ab8797ec8c0a8aedeaaf81b0d3f491ee
Sherry Sun <sherry.sun@nxp.com>
2022-05-27 14:35:54 +0800

    mxm_wifiex: update to mxm5x17322.p3 release
    Driver Bug Fixes:
    -----------------
    1. Added GPL/NXP dual license header
    2. WCSWREL-242: Fixed system rebooted issue after connecting wifi and running bt A2DP stress
    3. WCSWREL-310: merge the L5.17 build errors and warnings related to netif_rx
    4. WCSWREL-317: merge the build error and warning fix related to eth_hw_addr_set
    5. WSW-19664: Fixed VtsHalWifiV1_0TargetTest failure
    
    Signed-off-by: Sherry Sun <sherry.sun@nxp.com>

--- mxm_wifiex/wlan_src/mlinux/moal_shim.c
+++ mxm_wifiex/wlan_src/mlinux/moal_shim.c
@@ -5,18 +5,26 @@
  *
  * Copyright 2008-2022 NXP
  *
- * This software file (the File) is distributed by NXP
- * under the terms of the GNU General Public License Version 2, June 1991
- * (the License).  You may use, redistribute and/or modify the File in
- * accordance with the terms and conditions of the License, a copy of which
- * is available by writing to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA or on the
- * worldwide web at http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ * NXP CONFIDENTIAL
+ * The source code contained or described herein and all documents related to
+ * the source code (Materials) are owned by NXP, its
+ * suppliers and/or its licensors. Title to the Materials remains with NXP,
+ * its suppliers and/or its licensors. The Materials contain
+ * trade secrets and proprietary and confidential information of NXP, its
+ * suppliers and/or its licensors. The Materials are protected by worldwide
+ * copyright and trade secret laws and treaty provisions. No part of the
+ * Materials may be used, copied, reproduced, modified, published, uploaded,
+ * posted, transmitted, distributed, or disclosed in any way without NXP's prior
+ * express written permission.
  *
- * THE FILE IS DISTRIBUTED AS-IS, WITHOUT WARRANTY OF ANY KIND, AND THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE
- * ARE EXPRESSLY DISCLAIMED.  The License provides additional details about
- * this warranty disclaimer.
+ * No license under any patent, copyright, trade secret or other intellectual
+ * property right is granted to or conferred upon you by disclosure or delivery
+ * of the Materials, either expressly, by implication, inducement, estoppel or
+ * otherwise. Any license under such intellectual property rights must be
+ * express and approved by NXP in writing.
+ *
+ *  Alternatively, this software may be distributed under the terms of GPL v2.
+ *  SPDX-License-Identifier:    GPL-2.0
  *
  */
 
@@ -1600,14 +1608,15 @@ static mlan_status moal_recv_packet_to_mon_if(moal_handle *handle,
 		handle->mon_if->stats.rx_bytes += skb->len;
 		handle->mon_if->stats.rx_packets++;
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0)
-		netif_rx(skb);
-#else
 		if (in_interrupt())
 			netif_rx(skb);
 		else
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0)
+			netif_rx(skb);
+#else
 			netif_rx_ni(skb);
 #endif
+
 		status = MLAN_STATUS_PENDING;
 	}
 
