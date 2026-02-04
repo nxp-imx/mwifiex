f4fa645e7d962312d75752e240efa5ce91f72e96
Sherry Sun <sherry.sun@nxp.com>
2026-02-04 11:40:41 +0800

    mxm_wifiex: fix build error on L6.19 kernel next tree
    When build wifi driver based on L6.19 kernel next tree, will observe the
    following build error. It is caused by the upstream patch:
    2b5e9f9b7e414 ("net: Convert struct sockaddr to fixed-size "sa_data[14]"")
    Need to change corresponding API here to avoid the build break.
    
    Build error logs:
    mlinux/moal_eth_ioctl.c: In function ‘woal_priv_get_ap’:
    mlinux/moal_eth_ioctl.c:6970:55: error: ‘struct sockaddr’ has no member named ‘sa_data_min’; did you mean ‘sa_data’?
     6970 |                                 sizeof(mwr->u.ap_addr.sa_data_min));
          |                                                       ^~~~~~~~~~~
          |                                                       sa_data
    make[4]: *** [/scripts/Makefile.build:287: mlinux/moal_eth_ioctl.o] Error 1
    
    Signed-off-by: Sherry Sun <sherry.sun@nxp.com>
    Change-Id: I6996c73f58fb6d8d9a1d78460cdcd2614776a74b

--- mlinux/moal_eth_ioctl.c
+++ mlinux/moal_eth_ioctl.c
@@ -6966,7 +6966,8 @@ static int woal_priv_get_ap(moal_private *priv, t_u8 *respbuf, t_u32 respbuflen)
 	if (bss_info.media_connected == MTRUE) {
 		moal_memcpy_ext(priv->phandle, mwr->u.ap_addr.sa_data,
 				&bss_info.bssid, MLAN_MAC_ADDR_LENGTH,
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 80)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 80) &&                          \
+    LINUX_VERSION_CODE < KERNEL_VERSION(6, 19, 0)
 				sizeof(mwr->u.ap_addr.sa_data_min));
 #else
 				sizeof(mwr->u.ap_addr.sa_data));
